e075322aee561428f4004242afaf593007fe6573
Sanket Bhandare <sanket.s.bhandare@gmail.com>
2022-07-11 18:59:41 +0530

    new files added

---
+++ sanket/splice.js
@@ -0,0 +1,6 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// At position 2, add 2 elements: 
+fruits.splice(0, 0 , "pine","coco");
+
+document.getElementById("demo").innerHTML = fruits;
