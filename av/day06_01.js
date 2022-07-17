de536eb00a48dfe5d10c40d4cedb290a50466af4
abhayvel <quickitdotnet@gmail.com>
2022-07-17 14:01:39 +0530

    Added

--- av/day06_01.js
+++ av/day06_01.js
@@ -4,15 +4,18 @@ function convertDate(str) {
 	}
 	var dateArray = str.split('-');
 	if (dateArray.length == 3) {
-		return new Date((+dateArray[2]), (+dateArray[1])-1, (+dateArray[0]),0,0,0,0)
+
+		return new Date((+dateArray[2]), (+dateArray[1]) - 1, (+dateArray[0]), 0, 0, 0, 0);
 	}
 	return "not a Date"
 }
 
 	 //0 -> Jan
      //1->Feb
-var s = convertDate(new Date());
-console.log(s);
+//var s = convertDate(new Date());
+//console.log(s);
+
+console.log((new Date()).getTimezoneOffset())
 
 s = convertDate('01-01-2022');
-console.log(s);
+console.log(s +' ');
