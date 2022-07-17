917f2a94c500d0861cc1c69899d78ce736514da7
abhayvel <quickitdotnet@gmail.com>
2022-07-17 11:15:54 +0530

    added day06

--- demo2/.lintstagedrc.js
+++ demo2/.lintstagedrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  '*.{css,html,js,jsx,json,less,md,scss,ts,tsx,yml}': 'prettier --debug-check -c',
+  '*.{css,html,js,jsx,json,less,md,scss,ts,msx,yml}': 'prettier --debug-check -c',
   '*.{css,less,scss}': 'stylelint',
-  '*.{js,jsx,ts,tsx}': [
+  '*.{js,jsx,ts,msx}': [
     () => 'eslint', // https://github.com/okonet/lint-staged/issues/825#issuecomment-620018284
     'cross-env CI=true react-scripts test --bail --findRelatedTests --passWithNoTests',
   ],
