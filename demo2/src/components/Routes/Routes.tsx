492798cd6e1cb2e37e371ff6d9f6e1ec7526d6a3
Yogesh <ygaikwad690@gmail.com>
2022-07-23 11:04:05 +0530

    new changes

--- demo2/src/components/Routes/Routes.tsx
+++ demo2/src/components/Routes/Routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes as RoutesRRD } from 'react-router-dom';
 import { lazyComponent } from '../../common/utils';
+import { Department } from '../../pages/Student_Department_YG/Department';
 
 const Student = lazyComponent('Student', import('../../pages/Student_AV/Student'));
 const Home = lazyComponent('Home', import('../../pages/Home/Home'));
@@ -11,6 +12,7 @@ export const Routes = (): JSX.Element => (
         <Route element={<Student />} path="/" />
     <Route element={<Home />} path="/home" />
     <Route element={<PageNotFound />} path="*" />
+    <Route element={<Department />} path="/department" />
   </RoutesRRD>
 );
 
